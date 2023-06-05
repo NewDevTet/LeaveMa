1625faa8a3310259b22215b10891c9ea7fe5dadf
KNJ20 <karimanessane@gmail.com>
2023-06-05 18:17:22 +0100

    create project

---
+++ LeaveMa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveMa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
